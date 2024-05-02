d049ccdfec3976b6a9f532093ffc270257eb9947
Zahid-Hassan <zahid94dc@gmail.com>
2024-05-02 14:02:56 +0600

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Dash from "@/views/adminPanel/Dash.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/dash",
+      name: "dash",
+      component: Dash,
+      children: [
+        {
+          path: "/content",
+          name: "content",
+          component: () => import("../components/Content.vue"),
+        },
+        {
+          path: "/category",
+          name: "category",
+          component: () => import("../components/Category.vue"),
+        },
+        {
+          path: "/subcategory",
+          name: "subcategory",
+          component: () => import("../components/SubCategory.vue"),
+        },
+        {
+          path: "/brand",
+          name: "brand",
+          component: () => import("../components/Brand.vue"),
+        },
+        
+      ],
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
+
+export default router;
